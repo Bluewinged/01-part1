d455499acdf7bed1d66ed373aca442b50b115406
Bluewinged <kiki.kokkonen@outlook.com>
2024-08-16 10:05:57 +0300

    luotiin repo ja tuotiin tehtävän alku

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  console.log("Hello from komponentti");
+  return (
+    <>
+      <section>
+        <p>App-komponentissa on sisältö: Hello world!</p>
+      </section>
+    </>
+  );
+};
+
+export default App;
